98fc0c623aa3c96cd5ce9a2e4372c734ae936db3
waqasashiq31 <waqasashiq31@gmail.com>
2023-09-29 03:19:53 +0500

    Added room database and repository

---
+++ app/src/main/java/com/mobsol/notesapp/feature_note/data/data_source/NoteDao.kt
@@ -0,0 +1,25 @@
+package com.mobsol.notesapp.feature_note.data.data_source
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mobsol.notesapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * from note")
+    fun getNotes(): Flow<List<Note>>
+
+    @Query("SELECT * from note where id = :id")
+    suspend fun getNoteById(id: Int) : Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+}
